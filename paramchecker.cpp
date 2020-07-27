881f01e7db9fc210d4349afeb59a8366668704b0
kuldeepnigam83 <68851171+kuldeepnigam83@users.noreply.github.com>
2020-07-27 17:22:51 +0530

    updated saperate functions for bpm, spo2 and resprate

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,35 @@
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
+  if(checkBPM(bpm) && checkSPO2(spo2) && checkRespRate(respRate))
+  {
+    return true;
+  }
+   
+  return false;
+}
+
+bool checkBPM(float bpm)
+{
   if(bpm < 70 || bpm > 150) {
     return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
+  }
+  return true;
+}
+
+bool checkSPO2(float spo2)
+{
+  if(spo2 < 80) {
     return false;
   }
+  
+  return true;
+}
+
+bool checkRespRate(float respRate)
+{
+  if(respRate < 30 || respRate > 60) {
+    return false;
+  }
+  
   return true;
 }
