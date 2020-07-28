42bbb4d8bff4c621af2f6ca428f0ef32679d4134
kuldeepnigam83 <68851171+kuldeepnigam83@users.noreply.github.com>
2020-07-28 15:26:40 +0530

    repaces multiple vital funcn with single function

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,35 +1,23 @@
 
-bool isBPMOK(float bpm)
-{
-  if(bpm < 70 || bpm > 150) {
-    return true;
-  }
-  return false;
-}
+#define BPM_MIN 70
+#define BPM_MAX 150
+#define SPO2_MIN 0
+#define SPO2_MAX 80
+#define RESP_MIN 30
+#define RESP_MAX 60
 
-bool isSPO2OK(float spo2)
+
+bool isVitalOk(float vital, float minVal, float maxVal)
 {
-  if(spo2 < 80) {
-    return true;
+  if( (vital < minVal) || (vital > maxVal))
+  {
+    return false;
   }
   
-  return false;
-}
-
-bool isRespRateOK(float respRate)
-{
-  if(respRate < 30 || respRate > 60) {
-    return true;
-  }
-  
-  return false;
+  return true;
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
   
-  bool bpmFlag = isBPMOK(bpm);
-  bool spo2Flag =  isSPO2OK(spo2);
-  bool respRateFlag = isRespRateOK(respRate);
-  
-  return(bpmFlag && spo2Flag && respRateFlag);
+  return(isVitalOk(bpm, BPM_MIN, BPM_MAX) && isVitalOk(spo2, SPO2_MIN, SPO2_MAX) && isVitalOk(respRate, RESP_MIN, RESP_MAX));
 }
